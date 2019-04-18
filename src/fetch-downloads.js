29622ff8bba47dff2e1af59c7846ad8ac13b2836
Charles Stover <github@charlesstover.com>
2019-04-17 23:17:35 -0700

    cache invalidation fix

--- src/fetch-downloads.js
+++ src/fetch-downloads.js
@@ -1,51 +1,86 @@
 const fs = require('fs');
 const request = require('request');
 
-const YESTERDAY = Date.now() - 1000 * 60 * 60 * 24;
-
-const requestNodeModule = (resolve, reject, nodeModule, nodeModuleFilename) => {
-  request(
-    'https://api.npmjs.org/downloads/range/2017-02-25:9999-12-31/' + nodeModule,
-    (err, _response, body) => {
-      if (err) {
-        reject(err);
-        return false;
-      }
-      fs.writeFile('cache/' + nodeModuleFilename + '.json', body, err => {
-        if (err) {
-          console.error(err);
-        }
-      });
-      resolve(JSON.parse(body));
-      return true;
-    }
-  );
+const CATCH_ALL_PACKAGE = {
+  downloads: [ { downloads: 0 } ],
+  package: '@',
 };
 
-const fetchDownloads = (packages) =>
+const ONE_DAY = 1000 * 60 * 60 * 24;
+
+const log = (message, type = 'log') => {
+  console[type](`[${new Date().toISOString()}] ${message}.`);
+};
+
+const error = message => {
+  log(message, 'error');
+};
+
+const requestNodeModule = (nodeModule, nodeModuleFilename) => {
+  return new Promise((resolve, reject) => {
+    request(
+      'https://api.npmjs.org/downloads/range/2017-02-25:9999-12-31/' + nodeModule,
+      (err, _response, body) => {
+        if (err) {
+          error(`Error fetching ${nodeModule}: ${err.message}`);
+          reject(err);
+          return;
+        }
+        fs.writeFile('cache/' + nodeModuleFilename + '.json', body, err => {
+          if (err) {
+            error(
+              `Error saving cache file ${nodeModuleFilename}: ${err.message}`,
+            );
+          }
+        });
+        resolve(JSON.parse(body));
+      }
+    );
+  });
+};
+
+const fetchDownloads = packages =>
   Promise.all(
     packages.map(package =>
       Promise.all(
         package.map(nodeModule =>
           nodeModule === null ?
-            Promise.resolve({ downloads: [ { downloads: 0 } ], package: '@' }) :
+            Promise.resolve(CATCH_ALL_PACKAGE) :
             new Promise((resolve, reject) => {
-              const nodeModuleFilename = nodeModule.replace(/@/g, '__AT__').replace(/\//g, '__SCOPE__');
+              const nodeModuleFilename =
+                nodeModule.replace(/@/g, '__AT__').replace(/\//g, '__SCOPE__');
               fs.stat(
                 'cache/' + nodeModuleFilename + '.json',
                 (err, stats) => {
                   if (
                     err ||
-                    stats.mtimeMs < YESTERDAY
+                    stats.mtimeMs < Date.now() - ONE_DAY
                   ) {
-                    requestNodeModule(resolve, reject, nodeModule, nodeModuleFilename);
+                    if (err) {
+                      error(
+                        `Error reading cache file ${nodeModuleFilename}: ` +
+                        err.message,
+                      );
+                    }
+                    else {
+                      log(`Fetching ${nodeModule}.`);
+                    }
+                    requestNodeModule(nodeModule, nodeModuleFilename)
+                      .then(resolve)
+                      .catch(reject);
                   }
                   else {
                     fs.readFile(
                       'cache/' + nodeModuleFilename + '.json',
                       (err, data) => {
                         if (err) {
-                          requestNodeModule(resolve, reject, nodeModule, nodeModuleFilename);
+                          error(
+                            `Refecthing due to error reading package cache ` +
+                            ` for ${nodeModuleFilename}: ${err.message}`,
+                          );
+                          requestNodeModule(nodeModule, nodeModuleFilename)
+                            .then(resolve)
+                            .catch(reject);
                         }
                         else {
                           resolve(JSON.parse(data));
@@ -65,15 +100,27 @@ const fetchDownloads = (packages) =>
         const stats = Object.create(null);
         for (const packageStats of packagesStats) {
           const package = packageStats[0].package;
-          let downloads = 0;
+          stats[package] = [];
           for (const packageStat of packageStats) {
             if (Array.isArray(packageStat.downloads)) {
               for (const download of packageStat.downloads) {
-                downloads += download.downloads;
+                if (download.downloads > 0) {
+                  const DAYS_AGO = Math.floor(
+                    (Date.now() - new Date(download.day).getTime()) /
+                    ONE_DAY,
+                  );
+                  stats[package][DAYS_AGO] = download.downloads;
+                }
               }
             }
           }
-          stats[package] = downloads;
+
+          // Replace null with 0.
+          for (let day = 0; day < stats[package].length; day++) {
+            if (!stats[package][day]) {
+              stats[package][day] = 0;
+            }
+          }
         }
         return stats;
       }
--- src/index.js
+++ src/index.js
@@ -1,35 +1,42 @@
 const http = require('http');
 const fetchDownloads = require('./fetch-downloads');
-const packages = require('./packages');
 
 const headers = {
   'Access-Control-Allow-Methods': 'GET',
   'Access-Control-Allow-Origin': process.env.ACCESS_CONTROL_ALLOW_ORIGIN,
-  'Content-Type': 'application/json; charset=utf-8'
+  'Content-Type': 'application/json; charset=utf-8',
 };
 
+const ONE_MINUTE = 60000;
+const PACKAGES_PATH = './packages';
+const TWELVE_HOURS = 43200000;
+
 http.createServer((_request, response) => {
+  const packages = require(PACKAGES_PATH);
+  delete require.cache[require.resolve(PACKAGES_PATH)];
   fetchDownloads(packages)
     .then(json => {
       response.writeHead(200, {
         ...headers,
         'Cache-Control': 'max-age=43200, public', // 12 hours
-        'Expires': new Date(Date.now() + 43200000).toUTCString()
+        'Expires': new Date(Date.now() + TWELVE_HOURS).toUTCString(),
       });
       response.write(JSON.stringify(json));
       response.end();
     })
     .catch(err => {
+      console.error(err.message);
       response.writeHead(400, {
         ...headers,
         'Cache-Control': 'max-age=60, public', // 1 minute
-        'Expires': new Date(Date.now() + 60000).toUTCString()
+        'Expires': new Date(Date.now() + ONE_MINUTE).toUTCString()
       });
       response.write(JSON.stringify({
-        message: err.message
+        message: err.message,
       }));
       response.end();
     });
-}).listen(8080, () => {
-  console.log('Started successfully.');
-});
+})
+  .listen(8080, () => {
+    console.log('Started successfully.');
+  });
